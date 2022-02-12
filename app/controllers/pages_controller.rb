252458f62598d9983560830ab1f7bbdcad362605
Alisa Siegmund <alisa.siegmund@gmail.com>
2022-02-12 18:58:08 +0100

    clean up pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,85 +2,76 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    select_team
+    set_team
   end
 
   def order
     set_active_members
-    if !@team == nil?
+    if !@team.blank?
       @members = @active_members.shuffle
     end
   end
 
-
   def group
     set_active_members
-    if @team == nil?
-      @members = @active_members.shuffle
-    end
-
     @groupsize = params[:group_by]
 
     if params[:group_by].blank?
       @groups = []
     else
-      @groups = @active_members.shuffle.each_slice(@groupsize.to_i).to_a
+      if !@team.blank?
+        @groups = @active_members.shuffle.each_slice(@groupsize.to_i).to_a
+      end
     end
   end
-
 
   def pick
     set_active_members
-    if !@teams.nil?
+    if !@teams.blank?
       @member = @active_members.sample
     end
-
   end
 
   def surprise
-    @team = Team.all
+    # To be continued
   end
 
+# Define @TEAMS so it'll only contain teams of current user
   def set_teams
-    if Team.all.empty?
-      @message = "PLEASE CREATE TEAM"
+    if Team.all.where(user: current_user).blank?
+      @teams = []
     else
-      @teams = Team.all
+      @teams = Team.all.where(user: current_user)
     end
   end
 
+# Draw one @TEAM of @TEAMS on which the actions will be performed
   def set_team
     set_teams
 
-    if !@teams.nil?
-      if @teams.length == 1 || Selection.all.empty?
+    if !@teams.blank?
+      if @teams.length == 1 || Selection.all.blank?
         @team = Team.last
       else
         @team = Team.find(Selection.last.team_id)
       end
+    else
+      @team = []
     end
   end
 
+# Return @ACTIVE_MEMBERS only, so actions wont consider inactive members
   def set_active_members
     set_team
-    if !@team.nil?
-      @members = @team.member_ids
+    if !@team.blank?
+      members = @team.member_ids
       @active_members = []
 
-      @members.each do |member|
+      members.each do |member|
         if Member.find(member).active_member == true
           @active_members << member
         end
       end
-    end
-  end
-
-  def select_team
-     @teams = Team.all.where(user: current_user)
-    if @teams.length == 1 || Selection.all.empty?
-      @team = Team.last
-    else
-      @team = Team.find(Selection.last.team_id)
     end
   end
 
